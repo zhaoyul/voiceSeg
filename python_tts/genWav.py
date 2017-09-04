07b04c4019267005716dca2f9875a86adf9a2821
kevin.li <li_zhaoyu@msn.com>
2017-09-04 15:51:10 +0800

    增加语音合成提示音

---
+++ python_tts/genWav.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+import tts
+
+config_dict = {
+        'ko-KR': ['이폰（YeePhone） 자동번역 서비스를 이용해 주셔서 감사합니다. 발신자를 교체하려면 샵 버튼을 눌러주십시오.',
+                  '지금부터 한국어로 말씀하십시오.'],
+        'es-ES': ['Bienvenido al servicio de traducción automática de Yeephone, para cambiar de altavoz marque por favor el signo numeral.  ',
+                  'Por favor hable ahora en español.'],
+        'en-US': ['Thank you for using the interpreting services provided by YeePhone. Press the pound key to switch the speaker.  ',
+                  'Now please speak in English.  '],
+        'fr-FR': ['Bienvenue chez Yeephone avec ses services de traduction !  Appuyez sur la touche dièse pour changer de porte-parole ',
+                  'Maintenant prenez votre parole en français s\'il vous plaît !  '],
+        'ja-JP': ['YeePhone機械翻訳サービスをご利用いただきありがとうございます。 ',
+                  '日本語をお話ください。 '],
+        'it-IT': ['Benvenuti ad usare il servizio di traduzione automatica YeePhone, per cambiare il portavoce si prega di premere il tasto stemma ',
+                  'Ora si prega di parlare in italiano '],
+        'de-DE': ['Herzlich wilkommen in YeePhone Interpretation, bitte sprechen Sie und dann drücken Sie bitte die Reset-Taste.',
+                  'Jetzt spechen Sie bitte auf Deutsch.'],
+        'ru-RU': ['Добро пожаловать пользоваться переводной услугой YeePhone. Чтобы переходить на следующего выступающего, нажмите Знак решётки.',
+                  'Расскажите по-русски.'],
+        'zh-CN': ['欢迎使用译呼百应机器翻译服务，切换发言人请按井号键.',
+                  '现在请使用中文发言.'],
+        }
+
+def genLangArray(lang):
+    results = []
+    filtered_dict = {k:v for k,v in config_dict.items() if lang == k}
+    for lang, announce_array in filtered_dict.items():
+        for num, announce in enumerate(announce_array, start=1):
+            result_map = {}
+            result_map['src'] = announce
+            result_map['wav'] = lang+str(num)+'.wav'
+            results.append(result_map)
+    return results
+
+def writeLog(lang):
+    announce_array = genLangArray(lang)
+    file_object = open(lang+'.log', "w")
+    for announce in announce_array:
+        file_object.writelines([json.dumps(announce),'\n'] )
+
+if __name__ == '__main__':
+    accesstoken = tts.getToken()
+    for lang, announce_array in config_dict.items():
+        for num, announce in enumerate(announce_array, start=1):
+            print(accesstoken, lang, announce, lang+str(num)+'.wav')
+            tts.genWav(accesstoken, lang, announce, lang+str(num)+'.wav')
---
+++ python_tts/tts.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, json
+from xml.etree import ElementTree
+import sys
+
+def getToken():
+    params = ""
+    headers = {"Ocp-Apim-Subscription-Key": '006f1cd905e3457a8c565c50f9147a41'}
+
+    #AccessTokenUri = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+    AccessTokenHost = "api.cognitive.microsoft.com"
+    path = "/sts/v1.0/issueToken"
+
+    # Connect to server to get the Access Token
+    print ("Connect to server to get the Access Token")
+    conn = http.client.HTTPSConnection(AccessTokenHost)
+    conn.request("POST", path, params, headers)
+    response = conn.getresponse()
+    print(response.status, response.reason)
+
+    data = response.read()
+    conn.close()
+
+    accesstoken = data.decode("UTF-8")
+    #print ("Access Token: " + accesstoken)
+    return accesstoken
+
+def genWav(accesstoken, lang, text, file_name):
+    body = ElementTree.Element('speak', version='1.0')
+    body.set('{http://www.w3.org/XML/1998/namespace}lang', lang)
+    #body.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-us')
+    voice = ElementTree.SubElement(body, 'voice')
+    voice.set('{http://www.w3.org/XML/1998/namespace}lang', lang)
+    #voice.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-US')
+    voice.set('{http://www.w3.org/XML/1998/namespace}gender', 'Female')
+    if lang == 'zh-CN':
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (zh-CN, HuihuiRUS)')
+    if lang == 'en-US':
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)')
+    if lang == 'ja-JP':
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (ja-JP, HarukaRUS)')
+    if lang == 'ko-KR':
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (ko-KR, HeamiRUS)')
+    if lang == 'de-DE':
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (de-DE, HeddaRUS)')
+    if lang == 'fr-FR':
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (fr-FR, HortenseRUS)')
+    if lang == 'es-ES':
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (es-ES, HelenaRUS)')
+    if lang == 'ru-RU':
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (ru-RU, Irina, Apollo)')
+    voice.text = text
+    #voice.text = 'This is a demo to call microsoft text to speech service in Python.'
+
+    headers = {"Content-type": "application/ssml+xml",
+                "X-Microsoft-OutputFormat": "riff-16khz-16bit-mono-pcm",
+                "Authorization": "Bearer " + accesstoken,
+                "X-Search-AppId": "07D3234E49CE426DAA29772419F436CA",
+                "X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
+                "User-Agent": "TTSForPython"}
+
+    #Connect to server to synthesize the wave
+    print ("\nConnect to server to synthesize the wave")
+    conn = http.client.HTTPSConnection("speech.platform.bing.com")
+    conn.request("POST", "/synthesize", ElementTree.tostring(body), headers)
+    response = conn.getresponse()
+    print(response.status, response.reason)
+
+    data = response.read()
+    #file_name = 'output.wav'
+    with open(file_name, 'wb') as f:
+        f.write(data)
+
+    conn.close()
+    print("The synthesized wave length: %d" %(len(data)))
+if __name__ == '__main__':
+    lang=sys.argv[1]
+    text=sys.argv[2]
+    file_name=sys.argv[3]
+    accesstoken = getToken()
+    genWav(accesstoken, lang, text, file_name)
