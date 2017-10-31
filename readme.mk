699dfd4244d1d6c18a6b4233360fc7b3f30f77df
kevin.li <li_zhaoyu@msn.com>
2017-10-31 10:16:09 +0800

    更新 Readme

---
+++ readme.mk
@@ -0,0 +1,9 @@
+- 本地需要有python2、python3运行环境
+  - python2 > 2.7.1
+  - python3 > 3.6.1
+- 安装python2和python3的依赖
+  pip install -r py2requirment.txt
+  pip3 install -r py3requirment.txt
+- 配置uc.cfg
+- python sipua_manager
+
